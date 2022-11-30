75df0b10d9cbb075f1e6e1cff3157f2b005a0b59
Okefolahan Olamide <okefolahanolamide2006@gmail.com>
2022-11-30 22:42:29 +0100

    first commit

---
+++ static/javascript/script.js
@@ -0,0 +1,89 @@
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('#image-carousel', {
+        perPage: 4,
+        perMove: 1,
+        breakpoints: {
+            991: {
+                perPage: 2,
+            },
+            767: {
+                perPage: 2,
+            },
+            500: {
+                perPage: 1.5,
+            },
+        },
+    }).mount();
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('#image-carousel2', {
+        perPage: 4,
+        perMove: 1,
+        breakpoints: {
+            991: {
+                perPage: 2,
+            },
+            767: {
+                perPage: 2,
+            },
+            500: {
+                perPage: 1.5,
+            },
+        },
+    }).mount();
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('#image-carousel3', {
+        perPage: 4,
+        perMove: 1,
+        breakpoints: {
+            991: {
+                perPage: 2,
+            },
+            767: {
+                perPage: 2,
+            },
+            500: {
+                perPage: 1.5,
+            },
+        },
+    }).mount();
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('#image-carousel4', {
+        perPage: 4,
+        perMove: 1,
+        breakpoints: {
+            991: {
+                perPage: 2,
+            },
+            767: {
+                perPage: 2,
+            },
+            500: {
+                perPage: 1.5,
+            },
+        },
+    }).mount();
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('#image-carousel5', {
+        perPage: 4,
+        perMove: 1,
+        breakpoints: {
+            991: {
+                perPage: 2,
+            },
+            767: {
+                perPage: 2,
+            },
+            500: {
+                perPage: 1.5,
+            },
+        },
+    }).mount();
+});
